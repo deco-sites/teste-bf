0c3395c0b4319750a7ae621ef2ba0b8090c3612c
AbraaoMorais <moraisabraao@gmail.com>
2023-11-03 22:37:05 -0300

    FIX unclicable area

--- components/header/HeaderBf.tsx
+++ components/header/HeaderBf.tsx
@@ -17,7 +17,7 @@ export interface Props {
 
 function Drawer({ children }: { children: ComponentChildren }) {
     return (
-        <div className="drawer h-screen lg:hidden">
+        <div className="drawer lg:hidden">
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 {/* Page content here */}
